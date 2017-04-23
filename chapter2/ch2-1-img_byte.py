8985832c1b8e740a379d92d6181c22c9e4ba4a39
igomine <igomine@126.com>
2017-04-23 01:09:28 +0000

    learning opencv

---
+++ chapter2/ch2-1-img_byte.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy
+import os
+
+
+randomByteArray = bytearray(os.urandom(120000))
+flatNumpyArray = numpy.array(randomByteArray)
+
+
+grayImage = flatNumpyArray.reshape(300, 400)
+cv2.imwrite('RandomGray.png', grayImage)
+
+
+bgrImage = flatNumpyArray.reshape(100, 400, 3)
+cv2.imwrite('RandomColor.png',bgrImage)
